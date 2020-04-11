02c49b2f481a2afa50cb45966234881b54d62c30
pranjalikawale <pranjalikawale1578@gmail.com>
2020-04-11 07:03:45 -1200

    ADDED: First name validation

---
+++ UserRegistration.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+
+shopt -s extglob
+
+# check for name validation
+function nameValidation()
+{
+	if [[ $1 =~ $namePattern  ]]
+	then
+		echo "$1 is valid name"
+	else
+		echo "$1 is invalid name"
+	fi
+}
+# read name
+function readName()
+{
+	read name
+	echo $name
+}
+
+namePattern="^[A-Z]{1}[a-z]{2}$"
+echo "Enter the First Name"
+#invoke readName
+firstName="(readName)" 
+# invoke nameValidation 
+nameValidation $firstName
+
